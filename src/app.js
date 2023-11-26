3dff98f780d72c1659d2507c2153459fde6ab52e
Zaira <135705060+gilzaira@users.noreply.github.com>
2023-11-26 17:35:04 +0000

    first commit excuse generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generateExcuse();
 };
+function generateExcuse() {
+  let who = ["My dog", "My grandma", "My cat", "My spirit"];
+  let action = ["ate", "sat", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car", "my computer"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomAction = Math.floor(Math.random() * action.length);
+  let randomWhat = Math.floor(Math.random() * what.length);
+  let randomWhen = Math.floor(Math.random() * when.length);
+
+  return (
+    who[randomWho] +
+    " " +
+    action[randomAction] +
+    " " +
+    what[randomWhat] +
+    " " +
+    when[randomWhen]
+  );
+}
