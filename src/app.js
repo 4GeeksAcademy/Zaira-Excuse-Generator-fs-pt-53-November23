0bf7b8b9ce0cde35195f4ea41fbd2d6158a7faef
Zaira <135705060+gilzaira@users.noreply.github.com>
2024-01-07 17:43:11 +0000

    coding guidelines challenge first commit

--- src/app.js
+++ src/app.js
@@ -9,11 +9,12 @@ window.onload = function() {
   //write your code here
   document.querySelector("#excuse").innerHTML = generateExcuse();
 };
+
 function generateExcuse() {
-  let who = ["My dog", "My grandma", "My cat", "My spirit"];
-  let action = ["ate", "sat on", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car", "my computer"];
-  let when = [
+  const who = ["My dog", "My grandma", "My cat", "My spirit"];
+  const action = ["ate", "sat on", "crushed", "broke"];
+  const what = ["my homework", "the keys", "the car", "my computer"];
+  const when = [
     "before the class",
     "right on time",
     "when I finished",
@@ -21,10 +22,14 @@ function generateExcuse() {
     "while I was praying"
   ];
 
-  let randomWho = Math.floor(Math.random() * who.length);
-  let randomAction = Math.floor(Math.random() * action.length);
-  let randomWhat = Math.floor(Math.random() * what.length);
-  let randomWhen = Math.floor(Math.random() * when.length);
+  const getRandomIndex = array => Math.floor(Math.random() * array.length);
+
+  const [randomWho, randomAction, randomWhat, randomWhen] = [
+    getRandomIndex(who),
+    getRandomIndex(action),
+    getRandomIndex(what),
+    getRandomIndex(when)
+  ];
 
   return `${who[randomWho]} ${action[randomAction]} ${what[randomWhat]} ${when[randomWhen]}`;
 }
